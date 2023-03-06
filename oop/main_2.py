e220efd83f1303811934f1e107ad900c631806d4
Sayialel <124036970+Sayialel@users.noreply.github.com>
2023-03-06 15:23:51 +0300

    Add files via upload

---
+++ oop/main_2.py
@@ -0,0 +1,14 @@
+from main import vehicle
+
+
+vehicle_one = vehicle('Toyota','white','Kinuthia','four','1KZ')
+vehicle_two = vehicle('BMW','black','Rita','four','1KZ')
+vehicle_three = vehicle('Nissan','red','Dan','six','1KZ')
+
+
+
+
+
+print(vehicle_one.owner, vehicle_one.engine,vehicle_one.make, vehicle_one.color, vehicle_one.wheels)
+print(vehicle_two.owner, vehicle_two.engine,vehicle_two.make, vehicle_two.color, vehicle_two.wheels)
+print(vehicle_three.owner, vehicle_three.engine,vehicle_three.make, vehicle_three.color, vehicle_three.wheels)
