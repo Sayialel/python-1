d279a8e34f252971dcc364c2b6cf4b3a361cd0fd
Sayialel <124036970+Sayialel@users.noreply.github.com>
2023-03-06 15:53:34 +0300

    Add files via upload

---
+++ oop/house_template.py
@@ -0,0 +1,8 @@
+class house:
+
+    def __init__(self,type,door_color,roofing,tiles ):
+
+        self.type = type
+        self.door_color = door_color
+        self.roofing = roofing
+        self.tiles = tiles
---
+++ oop/house_template2.py
@@ -0,0 +1,8 @@
+from house_template import house
+
+house_A = house("Apartment","brown","brick","checked")
+house_B = house("Bungalow", "black", "iron sheets","blue")
+
+
+print(house_A.type,house_A.door_color,house_A.roofing,house_A.tiles)
+print(house_B.type,house_B.door_color,house_B.roofing,house_B.tiles)
